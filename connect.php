72d2fe6f2977bf597ccfd8a5c76a75c73310a494
Axel Tanner <axeltanner38@gmail.com>
2015-04-14 12:16:42 -0700

    milestone 4 created connection to mysqli database

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+	//passes(connects to) local host first, then username, then password, then database name
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+
+	//if database connection doesnt work...
+	if($mysqli->connect_error){
+		//want it to die and have this message:
+		die('Connect Error ('. $mysqli->connect_errno .')'
+			. $mysqli->connect_error);
+	}
+	//if connection made...
+	else{
+		//echoes out...
+		echo "Connection made";
+	}
+	//commands $mysqli to close
+	$mysqli->close();
+?>
