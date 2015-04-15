e34fcffcefb0f8d7294a0fef09f4c274f09472dd
Axel Tanner <axeltanner38@gmail.com>
2015-04-15 11:46:47 -0700

    milestone 6 setting up task

--- index.php
+++ index.php
@@ -6,7 +6,17 @@
 </head>
 <body>
 	<div class="wrap">
-		
+	<!--created task list-->
+		<div class="task-list">
+			<ul>
+				<!--requires connection to connect.php-->
+				<?php require("includes/connect.php"); ?>
+			</ul>
+		</div>
+		<!--form is going to add new task-->
+		<form class="add-new-task" autocomplete="off">
+			<input type="text" name="new-task" placeholder="Add new item..."/>
+		</form>
 	</div>
 </body>
 </html>
